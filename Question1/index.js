548cbd4086112b024a9fe14524128a2f21b342fa
Nitya Singh <76156577+NityaSG@users.noreply.github.com>
2023-07-21 15:57:21 +0530

    Added filters

--- Question1/index.js
+++ Question1/index.js
@@ -26,3 +26,60 @@ async function getToken() {
       console.error(error);
     }
   }
+
+  async function getAllTrains() {
+    try {
+      const response = await axios.get('http://20.244.56.144/train/trains', {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  function filterAndSortTrains(trains) {
+    const currentTime = new Date();
+    const twelveHoursLater = new Date(currentTime.getTime() + 12 * 60 * 60 * 1000);
+    return trains
+      .filter((train) => {
+        const departureTime = new Date(
+          currentTime.getFullYear(),
+          currentTime.getMonth(),
+          currentTime.getDate(),
+          train.departureTime.Hours,
+          train.departureTime.Minutes,
+          train.departureTime.Seconds
+        );
+        departureTime.setMinutes(departureTime.getMinutes() + train.delayedBy);
+        return (
+          departureTime > currentTime &&
+          departureTime < twelveHoursLater &&
+          departureTime > new Date(currentTime.getTime() + 30 * 60 * 1000)
+        );
+      })
+      .sort((a, b) => {
+        if (a.price.sleeper !== b.price.sleeper) return a.price.sleeper - b.price.sleeper;
+        if (a.seatsAvailable.sleeper !== b.seatsAvailable.sleeper)
+          return b.seatsAvailable.sleeper - a.seatsAvailable.sleeper;
+        const aDepartureTime = new Date(
+          currentTime.getFullYear(),
+          currentTime.getMonth(),
+          currentTime.getDate(),
+          a.departureTime.Hours,
+          a.departureTime.Minutes,
+          a.departureTime.Seconds
+        );
+        aDepartureTime.setMinutes(aDepartureTime.getMinutes() + a.delayedBy);
+        const bDepartureTime = new Date(
+          currentTime.getFullYear(),
+          currentTime.getMonth(),
+          currentTime.getDate(),
+          b.departureTime.Hours,
+          b.departureTime.Minutes,
+          b.departureTime.Seconds
+        );
+        bDepartureTime.setMinutes(bDepartureTime.getMinutes() + b.delayedBy);
+        return bDepartureTime - aDepartureTime;
+      });
+  }
+  
